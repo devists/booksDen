f97461f91bc085b291252db5939eed67e5c05c98
anilverma1996 <anilrajverma1996@gmail.com>
2016-02-02 10:38:42 +0530

    added dump file

---
+++ dev/booksden_book.sql
@@ -0,0 +1,51 @@
+-- phpMyAdmin SQL Dump
+-- version 4.1.14
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 02, 2016 at 05:50 AM
+-- Server version: 5.6.17
+-- PHP Version: 5.5.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `booksden`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `booksden_book`
+--
+
+CREATE TABLE IF NOT EXISTS `booksden_book` (
+  `isbn` int(13) NOT NULL,
+  `id` int(11) NOT NULL,
+  `up_date` timestamp NOT NULL,
+  `title` varchar(30) NOT NULL,
+  `author` varchar(20) NOT NULL,
+  `publisher` varchar(20) NOT NULL,
+  `p_year` timestamp NOT NULL,
+  `edition` int(5) NOT NULL,
+  `lang` varchar(10) NOT NULL,
+  `category` varchar(15) NOT NULL,
+  `tags` text NOT NULL,
+  `pages` int(7) NOT NULL,
+  `rating` varchar(10) NOT NULL,
+  `comment` text NOT NULL,
+  `count` int(5) NOT NULL,
+  PRIMARY KEY (`isbn`),
+  UNIQUE KEY `id` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
